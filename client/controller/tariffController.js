c5b3709fd33ed61134fdcfa55e7a225c04456e80
Neha Pant <neha.pant@niit.com>
2017-09-01 17:44:26 +0530

    1sept17 fri commit

--- client/controller/tariffController.js
+++ client/controller/tariffController.js
@@ -2,6 +2,16 @@ angular.module('meanApp').controller('tariffController', function($scope, $http)
 {
       $scope.tariffData="";
 
+$(document).ready(function(){
+  // $(".hr-time-picker").hrTimePicker();
+  $('#timepicker1').timepicki();
+  $('#timepicker2').timepicki();
+  // $('#timepicker3').timepicki();
+  // $('#timepicker4').timepicki();
+  });
+
+
+
     $scope.init = function(){
     $http.get('/tapi/GetTariff').success(function (response) {
       $scope.tariffData=response;
@@ -10,9 +20,12 @@ angular.module('meanApp').controller('tariffController', function($scope, $http)
   $scope.init();    
    
     $scope.SaveTariff = function() {
+      $scope.Tariff.StartPeakHour=$('#timepicker1').val();
+  $scope.Tariff.EndPeakHour=$('#timepicker2').val();
         $http.post('/tapi/AddTariff', $scope.Tariff).then(function(response) {
             console.log('Tariff data saved');
         });
+        window.location.reload();
     }
 
      $scope.DeleteTariff = function(tariff){
